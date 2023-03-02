7b39719eb7d991849bf47782c78b05242fdcfa34
MaxencePaulin <maxence.paulin25@gmail.com>
2023-03-02 09:57:42 +0100

    Initial Commit

---
+++ NoSleep/NoSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoSleepApp.swift
+//  NoSleep
+//
+//  Created by user236378 on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NoSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
