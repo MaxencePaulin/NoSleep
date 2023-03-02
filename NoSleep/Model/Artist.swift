022d28b8acb98ae5defadc88669981e5738d26bb
baptiste <baptiste.laval@edu.univ-fcomte.fr>
2023-03-02 14:45:36 +0000

    artist

--- NoSleep/Model/Artist.swift
+++ NoSleep/Model/Artist.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct Artist : Identifiable{
+    var id : Int
+    var photo : String
+    var name : String
+    var prename : String
+    var info : String
+    var style : String
+    var age : Int
+    
+    
+    static var dataArtist = [
+    Artist(id: 1 ,  photo: "PATH/photo" , name: "Hallyday" , prename:  "Jonny" , info: "chanteur qui chante " , style: "rock" , age: 90 )
+    
+    ]
+}
