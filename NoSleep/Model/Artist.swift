5a423f2afa4b54d7d06a18ab2afe242f7b7a7651
MaxencePaulin <maxence.paulin25@gmail.com>
2023-03-02 15:28:54 +0100

    init model

---
+++ NoSleep/Model/Artist.swift
@@ -0,0 +1,8 @@
+//
+//  Artist.swift
+//  NoSleep
+//
+//  Created by user236378 on 02/03/2023.
+//
+
+import Foundation
---
+++ NoSleep/Model/Concert.swift
@@ -0,0 +1,8 @@
+//
+//  Concert.swift
+//  NoSleep
+//
+//  Created by user236378 on 02/03/2023.
+//
+
+import Foundation
