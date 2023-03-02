49bf5dc04b6fde064476fe67213b73b4236eeaae
MaxencePaulin <maxence.paulin25@gmail.com>
2023-03-02 10:29:13 +0100

    test

--- NoSleep/ContentView.swift
+++ NoSleep/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("No Sleep !")
+        NavigationView() {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("No Sleep !")
+            }
+            .padding()
+            .navigationTitle("No Sleep")
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Text("test")
+                }
+            }
         }
-        .padding()
     }
 }
 
