d2004a37a958409eeffa4c4517a5f0267fa113ea
MaxencePaulin <maxence.paulin@edu.univ-fcomte.fr>
2023-03-31 09:15:40 +0200

    johnny fix

--- NoSleep/Model/Artist.swift
+++ NoSleep/Model/Artist.swift
@@ -18,18 +18,18 @@ struct Artist : Identifiable{
     
     
     static var artists = [
-        Artist(photo: "photo" , firstname:  "Jonny" , lastname: "Hallyday" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
-        Artist(photo: "photo" , firstname:  "Jonny2" , lastname: "Hallyday2" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
-        Artist(photo: "photo" , firstname:  "Jonny3" , lastname: "Hallyday3" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
-        Artist(photo: "photo" , firstname:  "Jonny4" , lastname: "Hallyday4" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
-        Artist(photo: "photo" , firstname:  "Jonny5" , lastname: "Hallyday5" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
-        Artist(photo: "photo" , firstname:  "Jonny6" , lastname: "Hallyday6" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
-        Artist(photo: "photo" , firstname:  "Jonny7" , lastname: "Hallyday7" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
-        Artist(photo: "photo" , firstname:  "Jonny8" , lastname: "Hallyday8" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
-        Artist(photo: "photo" , firstname:  "Jonny9" , lastname: "Hallyday9" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
-        Artist(photo: "photo" , firstname:  "Jonny10" , lastname: "Hallyday10" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
-        Artist(photo: "photo" , firstname:  "Jonny11" , lastname: "Hallyday11" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
-        Artist(photo: "photo" , firstname:  "Jonny12" , lastname: "Hallyday12" ,  info: "chanteur qui chante " , style: "rock" , age: 90 )
+        Artist(photo: "photo" , firstname:  "Johnny" , lastname: "Hallyday" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
+        Artist(photo: "photo" , firstname:  "Johnny2" , lastname: "Hallyday2" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
+        Artist(photo: "photo" , firstname:  "Johnny3" , lastname: "Hallyday3" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
+        Artist(photo: "photo" , firstname:  "Johnny4" , lastname: "Hallyday4" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
+        Artist(photo: "photo" , firstname:  "Johnny5" , lastname: "Hallyday5" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
+        Artist(photo: "photo" , firstname:  "Johnny6" , lastname: "Hallyday6" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
+        Artist(photo: "photo" , firstname:  "Johnny7" , lastname: "Hallyday7" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
+        Artist(photo: "photo" , firstname:  "Johnny8" , lastname: "Hallyday8" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
+        Artist(photo: "photo" , firstname:  "Johnny9" , lastname: "Hallyday9" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
+        Artist(photo: "photo" , firstname:  "Johnny10" , lastname: "Hallyday10" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
+        Artist(photo: "photo" , firstname:  "Johnny11" , lastname: "Hallyday11" ,  info: "chanteur qui chante " , style: "rock" , age: 90 ),
+        Artist(photo: "photo" , firstname:  "Johnny12" , lastname: "Hallyday12" ,  info: "chanteur qui chante " , style: "rock" , age: 90 )
         
     ]
 }
